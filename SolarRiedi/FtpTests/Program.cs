b9029a3bf900c3c14fb57f12408bff293b76db5c
Riedi Linus <linriedi@hotmail.com>
2016-09-01 22:16:54 +0200

    Use FtpClient to download file from ftp-server

--- SolarRiedi/FtpTests/Program.cs
+++ SolarRiedi/FtpTests/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ArxOne.Ftp;
+using System;
+using System.IO;
+using System.Net;
 
 namespace FtpTests
 {
@@ -10,6 +9,25 @@ namespace FtpTests
     {
         static void Main(string[] args)
         {
+            var uri = new Uri("ftp://solarriedi.ch/");
+            var credentials = new NetworkCredential("ftpsolarrie0b35", "Gy4A6EjYLy");
+
+            using (var ftpClient = new FtpClient(uri, credentials))
+            {
+                var stream = ftpClient.Retr("/httpdocs/min160901.csv");
+
+                using (var fileStream = File.Create("C://Users//linri//Desktop//test.csv"))
+                {
+                    byte[] buffer = new byte[8 * 1024];
+                    int len;
+                    while ((len = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fileStream.Write(buffer, 0, len);
+                    }
+                }
+
+                Console.WriteLine("");
+            }
         }
     }
 }
