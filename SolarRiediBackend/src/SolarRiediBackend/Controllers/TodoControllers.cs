a9a5fd019ddfe2c9ceb575a785328c18183c7a8a
Riedi Linus <linriedi@hotmail.com>
2016-09-11 17:12:14 +0200

    Add SolarRiediFrontEnd

---
+++ SolarRiediBackend/src/SolarRiediBackend/Controllers/TodoControllers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolarRiediBackend.Models;
+using System.Collections.Generic;
+
+namespace SolarRiediBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+
+        public ITodoRepository TodoItems { get; set; }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
