5d59bc3739f60050bd9efc76659508e597658995
Riedi Linus <linriedi@hotmail.com>
2016-09-11 22:34:31 +0200

    Add solution to prove tha databinding with ajax json to the jqxchards

---
+++ WebApplication1/src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return new JsonResult("[{ \"Day\": \"16:50:00\", \"Goal\": 23581 }, { \"Day\": \"16:45:00\", \"Goal\": 21000 }]");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
