0823cd615f46e69e7faff27a6f2d1accfd8dd3d6
Riedi Linus <linriedi@hotmail.com>
2016-10-14 23:51:27 +0200

    Add InitDatabase project

--- SolarRiediDataStoring/Settings/SettingsProvider.cs
+++ SolarRiediDataStoring/Settings/SettingsProvider.cs
@@ -8,22 +8,22 @@ namespace Linus.SolarRiedi.Settings
     {
         public string GetDbConnectionString()
         {
-            return null;
+            return "Server=tcp:solarriedi.database.windows.net,1433;Initial Catalog=SolarRiediDB;Persist Security Info=False;User ID=solarriedi;Password=MesjamnaNew2016;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         }
 
         public NetworkCredential GetFtpCredentials()
         {
-            return null;
+            return new NetworkCredential("ftpsolarrie0b35", "Gy4A6EjYLy");
         }
 
         public Uri GetFtpUri()
         {
-            return null;
+            return new Uri("ftp://solarriedi.ch/");
         }
 
         public string GetStorageConnectionString()
         {
-            return null;
+            return "DefaultEndpointsProtocol=https;AccountName=solarstorage;AccountKey=WI6Pkweae7xoqxI8KpUE37x+9yyHe+vAr+u3oIfcitpbjaG1QyCFfwV7/+uk4/PbdTcAlWUdF8NZMjAn8y4ZMA==";
         }
     }
 }
