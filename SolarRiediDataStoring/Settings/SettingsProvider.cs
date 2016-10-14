d6cd6b45003397751fac8b74e02bdae7882307c0
Riedi Linus <linriedi@hotmail.com>
2016-10-14 23:53:15 +0200

    Change SettingsProvider

--- SolarRiediDataStoring/Settings/SettingsProvider.cs
+++ SolarRiediDataStoring/Settings/SettingsProvider.cs
@@ -8,22 +8,22 @@ namespace Linus.SolarRiedi.Settings
     {
         public string GetDbConnectionString()
         {
-            return "Server=tcp:solarriedi.database.windows.net,1433;Initial Catalog=SolarRiediDB;Persist Security Info=False;User ID=solarriedi;Password=MesjamnaNew2016;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return null;
         }
 
         public NetworkCredential GetFtpCredentials()
         {
-            return new NetworkCredential("ftpsolarrie0b35", "Gy4A6EjYLy");
+            return null;
         }
 
         public Uri GetFtpUri()
         {
-            return new Uri("ftp://solarriedi.ch/");
+            return null;
         }
 
         public string GetStorageConnectionString()
         {
-            return "DefaultEndpointsProtocol=https;AccountName=solarstorage;AccountKey=WI6Pkweae7xoqxI8KpUE37x+9yyHe+vAr+u3oIfcitpbjaG1QyCFfwV7/+uk4/PbdTcAlWUdF8NZMjAn8y4ZMA==";
+            return null;
         }
     }
 }
