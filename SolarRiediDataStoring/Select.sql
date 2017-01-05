acd98d487db87895cb4a2588eb72c6dd132de0c0
Riedi Linus <linriedi@hotmail.com>
2017-01-05 17:34:48 +0100

    Extend Select command

--- SolarRiediDataStoring/Select.sql
+++ SolarRiediDataStoring/Select.sql
@@ -1,1 +1,3 @@
-﻿Select * from testIndex
+﻿Select Datum, Psum_1, Psum_2, Psum_3, Psum_4, Psum_5, Psum_6 from gis where datum >= 160900 And datum < 161000
+--Select * from gis where datum = 161126
+--Select * from gis where datum >= 161200 And datum < 170000
