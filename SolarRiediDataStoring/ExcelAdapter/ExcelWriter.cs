6fc32c56268f59ab856d469b15d482addc2f34e6
Riedi Linus <linriedi@hotmail.com>
2017-01-13 23:08:15 +0100

    clean up in ExcelWriter

--- SolarRiediDataStoring/ExcelAdapter/ExcelWriter.cs
+++ SolarRiediDataStoring/ExcelAdapter/ExcelWriter.cs
@@ -2,8 +2,6 @@
 using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 
 namespace ExcelAdapter
@@ -20,7 +18,6 @@ namespace ExcelAdapter
             var workbook = excel_app.Workbooks.Open(string.Format("{0}\\Template\\DayTemplate.xlsx", directory));
             var sheet = workbook.Worksheets["rawData"];
 
-            // Add some data to a range of cells.
             var values = new string[mesurementsInput.Count(), mesurementsInput.First().Count()];
             for(int i = 0; i < mesurementsInput.Count(); i++)
             {
@@ -34,72 +31,8 @@ namespace ExcelAdapter
             Range value_range = sheet.Range("A1", "H288");
             value_range.Value2 = values;
 
-            // Save the changes and close the workbook.
             workbook.Close(true, Type.Missing, Type.Missing);
-
-            // Close the Excel server.
             excel_app.Quit();
         }
-
-        //public void Write(IEnumerable<IEnumerable<string>> mesurementsInput)
-        //{
-        //    var mesurements = mesurementsInput.ToList();
-
-        //    Application oXL = null;
-        //    Workbook oWB = null;
-        //    Worksheet sheet = null;
-
-        //    try
-        //    {
-        //        var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
-        //        var directory = System.IO.Path.GetDirectoryName(path);
-
-        //        oXL = new Application();
-        //        oWB = oXL.Workbooks.Open(string.Format("{0}\\Template\\DayTemplate.xlsx", directory));
-
-        //        sheet = oWB.Worksheets["rawData"];
-
-        //        var colIndex = 1;
-
-        //        var nofRow = mesurements.Count();
-        //        var nofColumn = mesurements.First().Count();
-
-        //        for(int rowIndex = 0; rowIndex < 5; rowIndex++)
-        //        {
-        //            var row = mesurements[rowIndex];
-        //            colIndex = 1;
-        //            foreach (var col in row)
-        //            {
-        //                sheet.Cells[rowIndex + 1, colIndex] = col;
-        //                colIndex++;
-        //            }
-        //            Console.WriteLine(rowIndex);
-        //        }
-        //        oWB.Save();
-        //    }
-        //    finally
-        //    {
-        //        this.releaseObject(oXL);
-        //        this.releaseObject(oWB);
-        //        this.releaseObject(sheet);
-        //    }
-        //}
-
-        //private void releaseObject(object obj)
-        //{
-        //    try
-        //    {
-        //        System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
-        //        obj = null;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        obj = null;
-        //    }
-        //    finally
-        //    {
-        //        GC.Collect();
-        //    }
-        //}
     }
 }
