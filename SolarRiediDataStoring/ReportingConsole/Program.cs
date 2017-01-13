46ee55dae46a98634840fab95e5a554b9ec82800
Riedi Linus <linriedi@hotmail.com>
2017-01-13 18:02:25 +0100

    Add ReportingConsole project

---
+++ SolarRiediDataStoring/ReportingConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Date:");
+
+            var date = Console.ReadLine();
+        }
+    }
+}
